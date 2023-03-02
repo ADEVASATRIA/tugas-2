ec3d750e39a02465bd2f1c776eea283a5a4a869e
Adeva Satria Arif Wibawa <adevawibawa@gmail.com>
2023-03-02 23:28:33 +0700

    Tugas 2 Final

---
+++ Tugas_2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var temp float64
+	var scale string
+
+	fmt.Print("Enter temperature: ")
+	fmt.Scanln(&temp)
+
+	fmt.Print("Enter scale (C, F, K): ")
+	fmt.Scanln(&scale)
+
+	switch scale {
+	case "C":
+		fmt.Printf("%.2f°C = %.2f°F\n", temp, celsiusToFahrenheit(temp))
+		fmt.Printf("%.2f°C = %.2fK\n", temp, celsiusToKelvin(temp))
+	case "F":
+		fmt.Printf("%.2f°F = %.2f°C\n", temp, fahrenheitToCelsius(temp))
+		fmt.Printf("%.2f°F = %.2fK\n", temp, fahrenheitToKelvin(temp))
+	case "K":
+		fmt.Printf("%.2fK = %.2f°C\n", temp, kelvinToCelsius(temp))
+		fmt.Printf("%.2fK = %.2f°F\n", temp, kelvinToFahrenheit(temp))
+	default:
+		fmt.Println("Invalid scale. Please enter C, F, or K.")
+	}
+}
+
+func celsiusToFahrenheit(celsius float64) float64 {
+	return (celsius * 9 / 5) + 32
+}
+
+func celsiusToKelvin(celsius float64) float64 {
+	return celsius + 273.15
+}
+
+func fahrenheitToCelsius(fahrenheit float64) float64 {
+	return (fahrenheit - 32) * 5 / 9
+}
+
+func fahrenheitToKelvin(fahrenheit float64) float64 {
+	return (fahrenheit + 459.67) * 5 / 9
+}
+
+func kelvinToCelsius(kelvin float64) float64 {
+	return kelvin - 273.15
+}
+
+func kelvinToFahrenheit(kelvin float64) float64 {
+	return (kelvin * 9 / 5) - 459.67
+}
